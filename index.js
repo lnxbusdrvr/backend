071458bf28660578d4a832240696e72f60daea11
Erkki Pokkinen <erkki@vetokonsultit.fi>
2024-09-21 01:05:57 +0300

    Add MongoDB errorHandlers

--- index.js
+++ index.js
@@ -1,69 +1,82 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const Note = require('./models/note');
 
-// json-parseri
-app.use(express.json());
-app.use(cors());
-// käytetään stattisia sivuja(frontista tuotua)
+const Note = require('./models/note');
 app.use(express.static('dist'));
 
-// Määritellään customoitumorgan nimeltään
-// data
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorsHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const nonExistUrlHandler = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
 morgan.token('data', (request, response) => {
   return JSON.stringify(request.body);
 });
 
-// Otetaan customoitu morgan-nimeltään data käyttöön
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
-//let notes = [  {    id: "1",    content: "HTML is easy",    important: true  },  {    id: "2",    content: "Browser can execute only JavaScript",    important: false  },  {    id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  }];
+app.use(requestLogger);
 
-// GET
 app.get('/', (request, response) => {
   Note.find({}).then(notes => {
     respons.send('<h1>Hello Internet!</h1>');
   })
 });
 
-// GET
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-      response.json(note);
-  });
-});
-
-app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndDelete(request.params.id).then(result => {
-    if (result)
-      response.status(204).end();
-    else
-      response.status(404).end();
-  });
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 });
 
 
-// luodaan funktio, joka luo ID:n
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+});
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
 
-  // Varmistetaan, että kentässä "content" on arvo
+  // Varmistetaan, että kentässä "content" on läsnä
   if (!body.content)
     return response.status(400).json({ error: 'content missing' });
 
@@ -81,34 +94,29 @@ app.post('/api/notes', (request, response) => {
 });
 
 app.put('/api/notes/:id', (request, response) => {
-  const { content, important } = request.body; // Puretaan tarvittavat kentät pyynnön rungosta
+  const body  = request.body;
 
-  // Varmistetaan, että sisältö on annettu
-  if (!content) {
-    return response.status(400).json({ error: 'content missing' });
-  }
-
-  // Päivitettävä olio
   const note = {
-    content: content,
-    important: important || false, // Asetetaan tärkeä-arvo, jos se on annettu
+    content: body.content,
+    important: body.important,
   };
 
-  // Päivitetään dokumentti ID:n perusteella
-  Note.findByIdAndUpdate(
-    request.params.id,  // ID haetaan URL-parametreista
-    note,               // Päivityksen tiedot
-    { new: true }       // Asetus, joka palauttaa päivitetyn dokumentin
-  )
+  /* Päivitetään dokumentti ID:n perusteella
+   * ID haetaan URL-parametreista
+   * note = Päivityksen tiedot
+   * { new, true }  Asetus, joka palauttaa päivitetyn dokumentin
+   */
+  Note
+    .findByIdAndUpdate(request.params.id, note,{ new: true })
     .then(updatedNote => {
-      if (updatedNote) {
-        response.json(updatedNote);  // Palautetaan päivitetty dokumentti
-      } else {
-        response.status(404).end();  // Jos dokumenttia ei löydy
-      }
+      response.json(updatedNote);
     })
+    .catch(error => next(error));
 });
 
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(nonExistUrlHandler);
+app.use(errorsHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
