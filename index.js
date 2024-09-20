4fd59908f06addfdf8235b5c8677344e5f442b7d
Erkki Pokkinen <erkki@vetokonsultit.fi>
2024-09-21 02:44:22 +0300

    Add valitading error

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const errorsHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
@@ -63,7 +66,6 @@ app.get('/api/notes/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 });
-
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
